d9e50e4f1404d2049e5f6766560a5e2e7ecacef0
GiovanniBordignon <giovannibordignon@uol.com.br>
2023-02-13 19:59:55 -0300

    scrolls until find text

--- testCases/test_SearchingProduct_2.py
+++ testCases/test_SearchingProduct_2.py
@@ -1,6 +1,7 @@
 import time
 from appium import webdriver
 from selenium.webdriver.common.by import By
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import uiautomator2
@@ -32,27 +33,30 @@ time.sleep(7)
 """
 Still don't know how to make this work yet...
 
+Found the solution: 
+from appium.webdriver.common.appiumby import AppiumBy
+driver.find.element(AppiumBy.ANDROID_UIAUTOMATOR,'')
+
 driver.find_element_by_uiautomator('new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains("Air Huaraches").instance(0))')
 driver.find_element(By.uiautomator2,'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains("Air Huaraches").instance(0))').click()
 
-# scroll until find this -> Air Huaraches -> click
+# scroll until find this -> Text -> click
 """
-ScrollUtil.swipeUp(3, driver)
+
+# Scrolls down until find the element, then it perform the click
+driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains("Adidas Samba Vegan").instance(0))').click()
+
+#ScrollUtil.swipeUp(3, driver)
 
 """ 
 driver.swipe(600, 1700, 600, 500, 1000)
-driver.swipe(600, 1700, 600, 500, 1000)
-driver.swipe(600, 1700, 600, 500, 1000)
-driver.swipe(600, 1700, 600, 500, 1000)
-"""
 
-
-"""" 
 Y/vertical (top screen = 0, bottom screen = +/-2200)
 X/horizontal (left = 0, right = +/- 1000)
 
 To scroll in this specific case:
     Scroll Up = Y starts at 1700 and moves until 500)
 """
+
 time.sleep(2)
 driver.quit()
